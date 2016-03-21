7ec1fb915d89a1f812242cd462ceef77e9a1a258
HextechDarkling <hextechdarkling@gmail.com>
2016-03-21 13:33:49 -0400

    Added empty project files and basic .gitignore file.

---
+++ BLIP-controller/BLIP-controller/main.c
@@ -0,0 +1,18 @@
+/*
+ * BLIP-controller.c
+ *
+ * Created: 3/21/2016 1:23:40 PM
+ * Author : Alex
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
