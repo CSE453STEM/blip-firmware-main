c8990702cfbbd4d1ec755d3697b9450409274d40
Alex Ceci <hextechdarkling@gmail.com>
2016-03-25 13:36:07 -0400

    Added basic init functions for timers, gpio, and uart.  Added test function for uart

--- BLIP-controller/BLIP-controller/main.c
+++ BLIP-controller/BLIP-controller/main.c
@@ -7,10 +7,43 @@
 
 #include <avr/io.h>
 
+void init_gpio(void)
+{
+	//Init Port B
+	DDRB = 0xEF;	//Pins 0-6 output
+	PORTB = 0x00;	//All outputs off
+	
+	//Init port C
+	DDRC = 0xEF;	//Pins 0-6 output
+	PORTC = 0x00;	//All outputs off
+	
+	//Init port D
+	DDRD = 0x00;
+	PORTD = 0x00;
+}
+
+void init_timers(void)
+{
+	//Init timer 0
+	
+	//Init timer 1
+	
+	//Init timer 2
+	
+}
+
+void init_uart(void)
+{
+	UCSR0B = (1<<RXEN0)|(1<<TXEN0);
+	UBRR0 = 25;	//19200 baud
+}
 
 int main(void)
 {
-    /* Replace with your application code */
+	init_gpio();
+	init_uart();
+	
+	UDR0 = 'a';
     while (1) 
     {
     }
