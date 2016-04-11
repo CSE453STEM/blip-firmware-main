b615b30feef6fcc1714ce78160070a41cd132957
Daziana Bufford <dazianab@buffalo.edu>
2016-04-11 13:40:57 -0400

    leds

--- BLIP-controller/BLIP-controller/led.c
+++ BLIP-controller/BLIP-controller/led.c
@@ -4,3 +4,30 @@
  * Created: 3/25/2016 1:47:39 PM
  *  Author: Alex
  */ 
+
+
+
+/*this reads the leds*/
+unsigned char led_read(void){
+	unsigned char data;
+	
+	data = PINB; 
+	return data;
+}
+
+
+/*this turns the leds on */
+void led_write(unsigned char data){
+	
+	data = data & 0x7F;
+	PORTB = data;
+
+	
+}
+
+
+
+/*this clears the led lights*/
+void led_clear(void){
+	PORTB = 0;
+}
