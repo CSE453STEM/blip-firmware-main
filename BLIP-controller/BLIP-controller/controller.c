ed7411df5d464c1bf209d5b704668f0144391871
Alex Ceci <hextechdarkling@gmail.com>
2016-04-11 14:03:41 -0400

    Added controller header and source files

---
+++ BLIP-controller/BLIP-controller/controller.c
@@ -0,0 +1,6 @@
+/*
+ * controller.c
+ *
+ * Created: 4/11/2016 2:01:13 PM
+ *  Author: Alex
+ */ 
---
+++ BLIP-controller/BLIP-controller/controller.h
@@ -0,0 +1,16 @@
+/*
+ * controller.h
+ *
+ * Created: 4/11/2016 2:02:17 PM
+ *  Author: Alex
+ */ 
+
+
+#ifndef CONTROLLER_H_
+#define CONTROLLER_H_
+
+
+
+
+
+#endif /* CONTROLLER_H_ */
