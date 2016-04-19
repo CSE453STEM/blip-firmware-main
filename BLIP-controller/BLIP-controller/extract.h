a6d5f0801afaee0a982a9b21f5e4d1486a35d237
emvane <emvane@buffalo.edu>
2016-04-19 15:12:45 -0400

    header added

---
+++ BLIP-controller/BLIP-controller/extract.h
@@ -0,0 +1,11 @@
+#ifndef EXTRACT_H_
+#define EXTRACT_H_
+
+unsigned char message[128];                                                                  
+unsigned char count = 0;                                                                    
+unsigned char speed = 0;                                                                  
+unsigned char pay_size = 0;
+
+void buildmessage(unsigned char c);
+
+#endif
